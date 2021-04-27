3b320980bb76152b324c40afc9ecf73362b02872
Aderox <aderoxgame@gmail.com>
2021-04-27 15:35:59 +0200

    send message + select ONE guildChannel

--- index.js
+++ index.js
@@ -1,55 +1,133 @@
-const api = "https://discordapp.com/api/v8/users/@me/settings";
-const config = require("./config.json");
-const request = require("request");
+const api = "https://discordapp.com/api/v8/";
+const readline = require('readline');
+const fetch = require('node-fetch');
 
-const statusAnimation = statusAnimationSpin 
-const activity = "";
 
-let textAnimation = ""
-var size = Object.keys(statusAnimation.animation).length;
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+console.log('\x1b[36m%s\x1b[0m', 'I am cyan');  //cyan
+
+
+
+  function ask(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
 }
 
-var options = {
-    url: api,
-    method: "PATCH",
-    json: {
-        custom_status: {
-            text: textAnimation,
-            emoji_id: null,
-            emoji_name: null
-        }
-    },
-    headers: {
-        'Authorization': config.token
-    }
-  };
 
 
-async function loop(){
-for(let i=0; i<size;i++){
-    let timeout = statusAnimation.animation[i].timeout;
-    textAnimation = activity + "" +  statusAnimation.animation[i].text;
-    options.json.custom_status.text = textAnimation;
-    console.log("Text i:" + i +  " " + statusAnimation.animation[i].text);
-    //console.log(statusAnimation.animation[i].timeout);
-    await changeActivity();
-    await new Promise(resolve => setTimeout(resolve, timeout));
+async function connection(){
+    const email = await ask("email:\n>");
+    const password = await ask("password:\n>");
+
+    
+    
+    var tokenBody = {
+        "login":email,"password":password,"undelete":false,"captcha_key":null,"login_source":null,"gift_code_sku_id":null
+    };
+    var tokenHeaders = {
+        'Origin': "https://discord.com",
+        'Content-Type': 'application/json'
+    };
+
+    let TokenResponse = await fetch(api+"auth/login", {
+        method: "POST",
+        body: JSON.stringify(tokenBody),
+        headers: tokenHeaders,
+    })
+    let TokenResponseJson = await TokenResponse.json()
+    let token = TokenResponseJson.token
+    if(token != null && token != undefined){
+        console.log("Connecté !");
+        return token;
+    }else{
+        console.log("Erreur ! reconecté vous svp !");
+        return await connection();
     }
-    loop();
 }
 
-console.log("Let's funcking go...")
-loop();
+async function getUserGuild(token){
+
+    var headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    };
+    // /users/@me/guilds
+
+    let GuildReponse = await fetch(api+"users/@me/guilds", {
+        method: "GET",
+        headers: headers,
+    });
+    let GuildReponseJSON = await GuildReponse.json()    
+    return GuildReponseJSON;
+}
+
+async function getGuildChannel(token, guildID){
+    var headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    };
+    // /users/@me/guilds
+
+    let GuildChannels = await fetch(api+"guilds/" + guildID +"/channels", {
+        method: "GET",
+        headers: headers,
+    });
+    let GuildChannelsJSON = await GuildChannels.json()    
+    return GuildChannelsJSON;
+}
+
+async function main(){
+
+    let token = await connection();
+    let guildList = await getUserGuild(token);
+    console.log(guildList[0].name);
+    console.log("longeur: " + guildList.length);
 
 
+    for(let i=0; i<guildList.length; i++){
+        console.log("Guild n°" + i + ": " + guildList[i].name);
+    }
 
-async function changeActivity(){
-    request(options, function (error, response, body) {
-        //console.error('error:', error); // Print the error if one occurred
-        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //console.log('body.custom_status:', body); // Print the HTML for the Google homepage.
-      });
-};
-changeActivity();
+    let guildNumber = await ask("Guild: \n>");
+    let guildID = guildList[guildNumber].id;
+
+    let guildChannel = await getGuildChannel(token, guildID);
+
+    for(let i=0; i<guildChannel.length; i++){
+        console.log("Channel n°" + i + ": " + guildChannel[i].name);
+    }
+
+    let chanNumber = await ask("Guild: \n>");
+    let chanID = guildChannel[chanNumber].id;
+
+    let message = "";
+    while(message.toLowerCase() != "console.stop"){
+    message = await ask("message :\n>");
+    if(message.toLowerCase() == "console.stop"){
+        break;
+    }
+    var body = {
+        "content": message,
+        "tts": false
+    };
+    var headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+    };
+    fetch(api+"channels/"+chanID+"/messages", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: headers,
+        }).then(res => res.json()).then(json => {
+        //console.log(json);
+    });
+}
+}
+
+main();
